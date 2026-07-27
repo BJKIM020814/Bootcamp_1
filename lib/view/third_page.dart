5509d3d07cada51d22bed23c5c3970f7b20ad934
shjj8769 <shjj8769@gmail.com>
2026-07-27 16:53:24 +0900

    thpage

---
+++ lib/view/third_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:team_project_1/view/third_1_page.dart';
+
+class ThirdPage extends StatelessWidget {
+  const ThirdPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Third 3rd'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              }, 
+              child: Text('Go to the Screen #3'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Third1Page();
+                },));
+              }, 
+              child: Text('Go to the Screen #3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
