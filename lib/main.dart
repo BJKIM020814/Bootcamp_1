fbc68839f10eeb9f786bb180583904f3fefd6010
byeongjae <sweetday0814@gmail.com>
2026-07-27 15:25:49 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:team_project_1/view/first_page.dart';
+import 'package:team_project_1/view/second_page.dart';
+
+import 'view/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      routes: {
+        '/':(context) => Home(),
+        '/1st':(context) {
+          return FirstPage();
+        },
+        '/2nd':(context) => SecondPage()
+      },
+      initialRoute: '/',
+    );
+  }
+}
---
+++ lib/view/second_1_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Second1Page extends StatelessWidget {
+  const Second1Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
