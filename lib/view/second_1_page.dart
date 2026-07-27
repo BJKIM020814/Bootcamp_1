15816798c0a12c05d18e8eda862f627d4f8948a4
shjj8769 <shjj8769@gmail.com>
2026-07-27 16:00:52 +0900

    Second Image page

--- lib/view/second_1_page.dart
+++ lib/view/second_1_page.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:team_project_1/view/home.dart';
 
 class Second1Page extends StatelessWidget {
   const Second1Page({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second_1_Page'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage(
+                'images/파이리.jpg'
+              ),
+              radius: 50,
+            ),
+            ElevatedButton(
+              onPressed: () => Home(), 
+              child: Text('Go to Home'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                //
+              }, 
+              child: Text('Go to the Screen #2'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
